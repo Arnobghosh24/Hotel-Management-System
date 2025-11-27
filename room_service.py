372d6c1ed9ac389a823f228d77cf4267dd543d2b
Arnobghosh24 <120514819+Arnobghosh24@users.noreply.github.com>
2025-11-27 20:08:47 +0530

    Fix input validation and loop for room cleaning options

--- room_service.py
+++ room_service.py
@@ -3,6 +3,8 @@ import Main_data
 def program():
     print("Enter Exit to Log out")
     choice=input("Enter 1 to see how many rooms to clean. \nEnter 2 to see all the rooms to clean. \nEnter 3 to print which room to clean right now. \nEnter 4 to mark your job")
+    while(choice not in ["1","2","3","4","Exit"]):
+        choice=input("Invalid input")
     while(choice!="Exit"):
         if(choice=="1"):
             print(len(Main_data.room_cleaning))
@@ -22,9 +24,11 @@ def program():
                 Main_data.room_cleaning.remove(g_room)
             else:
                 print("Wrong Input")
-
-        else:
-            print("Invalid Input")
+        
+        print("Enter Exit to Log out")
+        choice=input("Enter 1 to see how many rooms to clean. \nEnter 2 to see all the rooms to clean. \nEnter 3 to print which room to clean right now. \nEnter 4 to mark your job")
+        while(choice not in ["1","2","3","4","Exit"]):
+            choice=input("Invalid input")
 
     print("\n\nLogging Out\n\n\n\n")
             
